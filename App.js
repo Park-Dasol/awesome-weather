a359e96616fffa2c7dc36ef6cfe231fc2e32d20b
dryans <dryans@naver.com>
2021-04-17 23:41:28 +0900

    feat : 날씨 정보 axios로 가져오기

--- App.js
+++ App.js
@@ -2,15 +2,32 @@ import React from "react";
 import Loading from "./Loading";
 import * as Location from 'expo-location';
 import {Alert} from "react-native";
+import axios from "axios";
+import { API_KEY }  from "@env"
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+  getWeather = async(latitude, longitude) => {
+    const {data} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      )
+    console.log(data)
+  }
+
+
+
   getLocation = async() => {
     try {
       await Location.requestForegroundPermissionsAsync();
       const {coords: {latitude, longitude}}= await Location.getCurrentPositionAsync();
+      // const location = await Location.getCurrentPositionAsync();
+      // console.log(location)
+      // console.log(coords.latitude, coords.longitue)
+      // altitude : 26.99516487121582
+      // longitude : 126.81262708382678
+      this.getWeather(latitude, longitude)
       this.setState({isLoading:false})
     } catch (error) {
       Alert.alert("Can't find you", "So sad");
